394999c8c6b64ff30a7698cfcc95f9a4bc469c5a
Sebastian Buchner <sebastian.buchner@pm.me>
2023-12-03 17:20:26 +0100

    finished Q1

--- 2023/03/Ratios.dart
+++ 2023/03/Ratios.dart
@@ -1,44 +1,43 @@
-import 'dart:ffi';
 import 'dart:io';
 import 'dart:core';
 
 void main() {
-  
   File file = new File('2023/03/input.txt');
   List<String> lines = file.readAsLinesSync();
-  
-  for(int currentLineIndex = 0; currentLineIndex < lines.length; currentLineIndex++) {
 
+  int sumValidParts = 0;
+
+  for (int currentLineIndex = 0;
+      currentLineIndex < lines.length;
+      currentLineIndex++) {
     String? prevLine; // null on first line
     String curLine;
     String? nextLine; // null on last line
 
-    if(currentLineIndex != 0) {
+    if (currentLineIndex != 0) {
       prevLine = lines[currentLineIndex - 1];
     }
     curLine = lines[currentLineIndex];
-    if(currentLineIndex + 1 != lines.length) {
+    if (currentLineIndex + 1 != lines.length) {
       nextLine = lines[currentLineIndex + 1];
     }
 
-    int sumValidPartNumbers = getSumValidPartsCurrentLine(prevLine, curLine, nextLine);
-
+    sumValidParts += getSumValidPartsCurrentLine(prevLine, curLine, nextLine);
   }
 
+  print("Answer for Q1 is: " + sumValidParts.toString());
 }
 
 /**
  * Returns all valid numbers in current line
  */
-int getSumValidPartsCurrentLine(String? prevLine, String curLine, String? nextLine) {
-
+int getSumValidPartsCurrentLine(
+    String? prevLine, String curLine, String? nextLine) {
   List<BeginEndIndex> beginEndIndexList = getBeginAndEndIndex(curLine);
   int sumValidPartNumbers = 0;
-  
 
   // get sourrinding chars of number in current line
-  for(BeginEndIndex beginEndIndex in beginEndIndexList) {
-
+  for (BeginEndIndex beginEndIndex in beginEndIndexList) {
     int beginIndex = beginEndIndex.getBeginIndex();
     int endIndex = beginEndIndex.getEndIndex() + 1;
 
@@ -46,29 +45,29 @@ int getSumValidPartsCurrentLine(String? prevLine, String curLine, String? nextLi
     String surroundingCurLine = "";
     String surroundingNextLine = "";
 
-    if(prevLine != null) {
+    if (prevLine != null) {
       surroundingPrevLine = prevLine.substring(beginIndex, endIndex);
     }
     surroundingCurLine = curLine.substring(beginIndex, endIndex);
-    if(nextLine != null) {
+    if (nextLine != null) {
       surroundingNextLine = nextLine.substring(beginIndex, endIndex);
     }
 
     // contains all chars surrounding number in current line
-    String surroundingNumber = surroundingPrevLine + surroundingCurLine + surroundingNextLine;
+    String surroundingNumber =
+        surroundingPrevLine + surroundingCurLine + surroundingNextLine;
 
-    sumValidPartNumbers += computeValidPartNumbers(surroundingNumber, surroundingCurLine);
+    sumValidPartNumbers +=
+        computeValidPartNumbers(surroundingNumber, surroundingCurLine);
   }
 
   return sumValidPartNumbers;
-
-} 
+}
 
 /**
  * Returns all begin and end indices of numbers of a String
  */
 List<BeginEndIndex> getBeginAndEndIndex(String curLine) {
-
   List<BeginEndIndex> beginEndIndexList = new List.empty(growable: true);
 
   RegExp digitRegExp = new RegExp(r'\d');
@@ -76,65 +75,64 @@ List<BeginEndIndex> getBeginAndEndIndex(String curLine) {
   int? beginIndex; // stores also first '.'
   int? endIndex; // stores also last '.'
 
-  bool lookingForBeginIndex = true; 
+  bool lookingForBeginIndex = true;
   bool foundNumber = false;
 
-  for(int currentCharIndex = 0; currentCharIndex < curLine.length; currentCharIndex++) {
-
-    print(currentCharIndex);
-
+  for (int currentCharIndex = 0;
+      currentCharIndex < curLine.length;
+      currentCharIndex++) {
     // search for index of begin and end of number
-    if(curLine[currentCharIndex].contains(digitRegExp) && lookingForBeginIndex) {
+    if (curLine[currentCharIndex].contains(digitRegExp) &&
+        lookingForBeginIndex) {
       beginIndex = (currentCharIndex == 0) ? 0 : (currentCharIndex - 1);
       lookingForBeginIndex = false; // start search for end of number
     }
-    if((!curLine[currentCharIndex].contains(digitRegExp) && !lookingForBeginIndex) || (currentCharIndex == 1)) {
+    if ((!curLine[currentCharIndex].contains(digitRegExp) &&
+            !lookingForBeginIndex) ||
+        (currentCharIndex == (curLine.length - 1) && !lookingForBeginIndex)) {
       endIndex = currentCharIndex;
       lookingForBeginIndex = true; // start search for new number
       foundNumber = true;
     }
 
     // has found number
-    if(foundNumber) {
-      BeginEndIndex beginEndIndexEntry = new BeginEndIndex(beginIndex!, endIndex!);
+    if (foundNumber) {
+      BeginEndIndex beginEndIndexEntry =
+          new BeginEndIndex(beginIndex!, endIndex!);
       beginEndIndexList.add(beginEndIndexEntry);
       foundNumber = false;
     }
-
   }
 
   return beginEndIndexList;
-
 }
 
 /**
  * Returns valid part numbers or 0 if part number is invalid
  */
-int computeValidPartNumbers(String surroundingNumber, String surroundingCurLine) {
-
+int computeValidPartNumbers(
+    String surroundingNumber, String surroundingCurLine) {
   RegExp validRegExp = new RegExp(r'\d');
 
   bool isValidPart = false;
-  for(int cur = 0; cur < surroundingNumber.length; cur++) {
+  for (int cur = 0; cur < surroundingNumber.length; cur++) {
     // check if there is any character that doesnt match digit or '.'
-    if(!surroundingNumber[cur].contains(validRegExp) && surroundingNumber[cur] != '.') {
+    if (!surroundingNumber[cur].contains(validRegExp) &&
+        surroundingNumber[cur] != '.') {
       isValidPart = true;
       break;
     }
   }
 
-  if(isValidPart) {
-    String number = surroundingCurLine.replaceAll(new RegExp(r'[^0-9]'),'');;
-    print("Number: " + number);
+  if (isValidPart) {
+    String number = surroundingCurLine.replaceAll(new RegExp(r'[^0-9]'), '');
     return int.parse(number);
   }
 
   return 0;
-  
 }
 
 class BeginEndIndex {
-
   int? beginIndex;
   int? endIndex;
 
@@ -144,17 +142,16 @@ class BeginEndIndex {
   }
 
   int getBeginIndex() {
-    if(beginIndex != null) {
+    if (beginIndex != null) {
       return beginIndex!;
     }
     throw "Invalid index found";
   }
 
   int getEndIndex() {
-    if(endIndex != null) {
+    if (endIndex != null) {
       return endIndex!;
     }
     throw "invalid index found";
   }
-
 }
